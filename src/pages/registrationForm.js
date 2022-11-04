d02981653baaa70f27b139a4e48269c4ef498eea
Nasuco <2011501026@student.unisayogya.ac.id>
2022-11-04 17:58:46 +0700

    first commit

---
+++ src/pages/registrationForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegistrationForm from '../components/RegistrationForm/registrationForm';
+
+const RegistrationForm = () => {
+    return (
+        <div>
+            <RegistrationForm />
+        </div>
+    )
+}
+
+export default RegistrationForm;
